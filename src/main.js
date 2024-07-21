f8232d11c5617555e7c54d70ad3c3c10bf9e535d
Brianna Wang <wang.brianna@gmail.com>
2024-07-21 19:56:52 -0400

    added happiness attribute

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 let addToDoButton = document.getElementById('addToDo');
 let listContainer = document.getElementById('list-container');
 let inputField = document.getElementById('inputField');
-let coinCount = document.getElementById('coinCount')
+let coinCount = document.getElementById('coinCount');
 let coins = 0;
+let happApp = document.getElementById('happApp');
+
+const progress = document.getElementById('p1');
+let percent = 0;
 
 function addTask(){
     if(inputField.value == "") {
@@ -23,8 +27,14 @@ function addTask(){
 function updateCoins(num){
     coins = coins + num
     coinCount.innerHTML = coins;
-    console.log(coinCount.innerHTML)
+    //console.log(coinCount.innerHTML)
 
+}
+
+function changeWidth(){
+    percent = percent + 10
+    progress.style.width = `${percent}%`;
+    progress.innerText = `${percent}%`;
 }
 listContainer.addEventListener("click", function(e){
     if(e.target.tagName === "LI"){
@@ -45,20 +55,31 @@ listContainer.addEventListener("click", function(e){
     }
 }, false);
 
+happApp.addEventListener("click", function(e) {
+    if(coins >= 100){
+        updateCoins(-100);
+        changeWidth();
+    }else{
+        alert("you don't have enough coins!");
+    }
+}, false);
+
+
 function saveData(){
     localStorage.setItem("tasks", listContainer.innerHTML);
     localStorage.setItem("coins", coinCount.innerHTML);
-    console.log("saved")
-    console.log(coins.innerHTML);
+    //console.log("saved")
+    //console.log(coins.innerHTML);
 
 }
 
 function showData(){
     listContainer.innerHTML = localStorage.getItem("tasks");
     coinCount.innerHTML = localStorage.getItem("coins");
-    console.log("got")
-    console.log(coins.innerHTML);
+    //console.log("got")
+    //console.log(coins.innerHTML);
 
 }
 
 showData();
+console.log(progress.style.width);
